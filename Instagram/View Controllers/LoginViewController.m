ed708766eb6735f9356a24f479f14ad32632b669
Brian Sanchez <brian36@fb.com>
2020-07-06 15:04:43 -0700

    added sign up button (not functional yet)

--- Instagram/View Controllers/LoginViewController.m
+++ Instagram/View Controllers/LoginViewController.m
@@ -8,7 +8,9 @@
 
 #import "LoginViewController.h"
 
-@interface ViewController ()
+@interface LoginViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *usernameField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordField;
 
 @end
 
@@ -18,6 +20,17 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
 }
+- (IBAction)pressSignUp:(id)sender {
+}
 
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
 
 @end
