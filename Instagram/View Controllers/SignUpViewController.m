ace7c855a3b73b6ea08446877854b00c13a93743
Brian Sanchez <brian36@fb.com>
2020-07-06 15:27:18 -0700

    added sign up view controller

---
+++ Instagram/View Controllers/SignUpViewController.m
@@ -0,0 +1,42 @@
+//
+//  SignUpViewController.m
+//  Instagram
+//
+//  Created by Brian Sanchez on 7/6/20.
+//  Copyright © 2020 Brian Sanchez. All rights reserved.
+//
+
+#import "SignUpViewController.h"
+#import <Parse/Parse.h>
+
+@interface SignUpViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *usernameField;
+@property (weak, nonatomic) IBOutlet UITextField *emailField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordField;
+
+@end
+
+@implementation SignUpViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+- (IBAction)pressSignUp:(id)sender {
+    
+}
+- (IBAction)pressCancel:(id)sender {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
